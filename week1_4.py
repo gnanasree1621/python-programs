f59e7d4d9033c57af2fe32868b759cd32cf3ee35
vittanala Gnana sree <vittanalagnanasree@gmail.com>
2025-09-30 14:11:19 +0530

    Add files via upload

---
+++ week1_4.py
@@ -0,0 +1,9 @@
+>>> numbers=eval(input("enter list:"))
+enter list:[34,67,36,2]
+>>> largest=min(numbers)
+>>> print("the largest number in the list:",largest)
+the largest number in the list: 67
+>>> 
+
+>>> 
+================================================================ RESTART: C:/Users/Gnanasree/OneDrive/Desktop/week1_5.py =======================================================
