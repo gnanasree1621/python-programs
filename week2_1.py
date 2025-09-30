946cbb58cec2ac1ad19b9b1ca3796ab4b4e66a9f
vittanala Gnana sree <vittanalagnanasree@gmail.com>
2025-09-30 14:12:27 +0530

    Add files via upload

---
+++ week2_1.py
@@ -0,0 +1,10 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Std:
+...     rollno=490
+...     name="gnana sree"
+...     def display(self):
+...         print("Roll No:",self.rollno)
+...         print("Name:",self.name)
+... s1=Std()
+... s1.display()
